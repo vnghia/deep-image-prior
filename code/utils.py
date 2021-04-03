f19879002226575368d63e5f75aa475e73c58f73
Võ Văn Nghĩa <vovannghia2409@gmail.com>
2021-04-03 22:08:38 +0200

    fix plot_img

--- code/utils.py
+++ code/utils.py
@@ -26,7 +26,7 @@ def load_img(path, data_format=None, dtype=tf.float32):
     data_format = convert_data_format(data_format, False)
     img = tf.keras.preprocessing.image.load_img(path)
     img = tf.keras.preprocessing.image.img_to_array(img, data_format)
-    return tf.image.convert_image_dtype(img, dtype)
+    return tf.math.divide(img, 256.0)
 
 
 def save_img(path, img, data_format=None):
@@ -37,13 +37,21 @@ def save_img(path, img, data_format=None):
 
 def plot_img(imgs, nrows=None, ncols=3, figsize=(5, 5)):
     if tf.rank(imgs) == 3:
-        imgs = [imgs]
+        imgs = tf.expand_dims(imgs, axis=0)
     nimgs = tf.shape(imgs)[0]
-    nrows = nimgs // ncols + 1 if nrows is None else nrows
-    _, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=figsize)
+    nrows = int(tf.math.ceil(nimgs / ncols)) if nrows is None else nrows
+    _, axes = plt.subplots(
+        nrows=nrows,
+        ncols=ncols,
+        figsize=figsize,
+        sharex=True,
+        sharey=True,
+        squeeze=False,
+    )
     for i, img in enumerate(imgs):
         axes.ravel()[i].imshow(img)
         axes.ravel()[i].set_axis_off()
+        axes.ravel()[i].set_aspect(1)
     plt.tight_layout()
     plt.show()
 
