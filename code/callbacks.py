906f17d2757fa52b013be5ce6fb829deceb9fde5
Võ Văn Nghĩa <vovannghia2409@gmail.com>
2021-04-04 22:41:16 +0200

    don't keep output by default

--- code/callbacks.py
+++ code/callbacks.py
@@ -6,7 +6,15 @@ import utils
 
 
 class SavePredictions(tf.keras.callbacks.Callback):
-    def __init__(self, input_net, step=100, rootdir=None, plot=False, data_format=None):
+    def __init__(
+        self,
+        input_net,
+        step=100,
+        rootdir=None,
+        plot=False,
+        data_format=None,
+        keep_output=False,
+    ):
         super().__init__()
         self.input_net = input_net
         self.outputs_net = []
@@ -14,6 +22,7 @@ class SavePredictions(tf.keras.callbacks.Callback):
         self.rootdir = rootdir
         self.plot = plot
         self.data_format = data_format
+        self.keep_output = keep_output
 
     def on_epoch_end(self, epoch, logs=None):
         if epoch % self.step != 0:
@@ -25,6 +34,7 @@ class SavePredictions(tf.keras.callbacks.Callback):
                 result,
                 self.data_format,
             )
-        self.outputs_net.append(result)
+        if self.keep_output:
+            self.outputs_net.append(result)
         if self.plot:
             utils.plot_img(result, ncols=1, data_format=self.data_format)
